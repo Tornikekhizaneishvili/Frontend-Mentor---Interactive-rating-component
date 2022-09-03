b3c985bfc086f81f65d1dcd525df217fd0f52a3e
Tornikekhizaneishvili <khizaneishvilitornike@gmail.com>
2022-09-03 19:12:44 +0400

    interactive-rating-component

---
+++ app.js
@@ -0,0 +1,19 @@
+let wichButtonClicked = 0;
+let btn = document.getElementsByClassName("submit-button");
+
+
+
+
+
+function reply_click(clicked_id) {
+  alert(clicked_id);
+
+  wichButtonClicked = clicked_id;
+
+
+}
+
+console.log(wichButtonClicked);
+
+
+
